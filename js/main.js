f2eaa2d0ea6fa4a869845ebbf2d8c3f9f5709e03
Dj-Rom <118632628+Dj-Rom@users.noreply.github.com>
2023-04-14 21:07:13 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,67 +1,55 @@
 "use strict";
-function HashStorageFunc() { }
-HashStorageFunc.prototype.addValue = function (key, value) { return this[key] = value; }
-HashStorageFunc.prototype.getValue = function (key) { return this[key] }
-HashStorageFunc.prototype.deleteValue = function (key) {
-   let allKeys = Object.keys(this)
-   let result = Boolean
-   for (let i = 0; i < allKeys.length; i++) {
-      result = (allKeys[i] == key) ? true : false
-      if (result) { return delete this[key]; }
-   } return result
+
+function HashStorageFunc() {}
+HashStorageFunc.prototype.addValue = function (key, value) {
+   return this[key] = value;
 }
-HashStorageFunc.prototype.getKeys = function (key) { return Object.keys(this) }
+HashStorageFunc.prototype.getValue = function (key) {
+   return this[key]
+}
+HashStorageFunc.prototype.deleteValue = function (key) {
+
+   delete this[key]
+
+}
+HashStorageFunc.prototype.getKeys = function (key) {
+   return Object.keys(this)
+}
 const drinkStorage = new HashStorageFunc();
 const nameDrink = document.querySelector('#enterDrink');
 nameDrink.addEventListener('click', () => {
-   const keyDrink = prompt('Введите название напитка')
-   let alcoholDrinkUser1 = ' '
-   const alcoholDrinkUser = confirm(' напиток алкогольный ок=ДА, отмена=НЕТ')
-   if (alcoholDrinkUser) { alcoholDrinkUser1 = 'да' }
-   else { alcoholDrinkUser1 = 'нет' }
-   const valueDrinkUser = prompt('рецепт его приготовления')
+   const keyDrink = prompt('Введите название напитка');
+   let alcoholDrinkUser1 = ' ';
+   const alcoholDrinkUser = confirm(' напиток алкогольный ок=ДА, отмена=НЕТ');
+   (alcoholDrinkUser)?alcoholDrinkUser1 = 'да': alcoholDrinkUser1 = 'нет';
+   const valueDrinkUser = prompt('рецепт его приготовления');
    const drink = {
       recept: valueDrinkUser,
       alcohol: alcoholDrinkUser1
    }
    drinkStorage.addValue(keyDrink.toLowerCase(), drink);
 });
-console.log(drinkStorage);
+
 const resultDrink = document.querySelector('#resultDrink');
 resultDrink.addEventListener('click', () => {
    const allInfoDrink = prompt('«получение информации о напитке»');
-   const g = (drinkStorage.getValue(allInfoDrink))
+   const g = (drinkStorage.getValue(allInfoDrink));
+   (Object.keys(drinkStorage).includes(allInfoDrink)) ?
    alert(`напиток ${allInfoDrink}
-алкогольный: ${g.alcohol}
-рецепт приготовления:
- ${g.recept}`)
+   алкогольный: ${g['alcohol']}
+   рецепт приготовления:
+   ${g['recept']}`): alert(`not ${allInfoDrink}`);
+
 });
 const deleteDrink = document.querySelector('#deleteDrink');
 deleteDrink.addEventListener('click', () => {
    const deleteNameDrink = prompt('«удаление информации о напитке»');
-   drinkStorage.deleteValue(deleteNameDrink)
+   (Object.keys(drinkStorage).includes(deleteNameDrink))  ? alert(`${deleteNameDrink} удаленно успешно `) & drinkStorage.deleteValue(deleteNameDrink):alert("Ошибка");
 });
 const allDrink = document.querySelector('#allDrink');
-allDrink.addEventListener('click', () => { alert(`«перечень всех напитков» ${drinkStorage.getKeys()} `); });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+allDrink.addEventListener('click', () => {
+   alert(`«перечень всех напитков» ${drinkStorage.getKeys()}`);
+});
 
 
 
